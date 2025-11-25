070d16ec3fb465f71b03202881d1700fd78d0491
Antonio-Par <polikarpez@gmail.com>
2025-11-25 20:38:49 +0300

    Worked a bit )

--- threads-2-16-3/main.py
+++ threads-2-16-3/main.py
@@ -13,14 +13,33 @@ class ConcurrentPoolExecutor:
     def __init__(self, max_workers=1):
         self.max_workers = max_workers
         self.func_info = []
-        self.results = None
+        self.futures = []
+        self.results = []
         self.cpe = fut.ThreadPoolExecutor(max_workers=self.max_workers)
         
     def concurrent_run(self, func: Callable) -> Callable:
         def wrapper(*args, **kwargs):
             self.func_info = f'{func.__name__}{args}'
-            self.cpe.submit(func, *args)
+            try:
+                self.futures.append(self.cpe.submit(func, *args))
+                done, _ = fut.wait(self.futures, timeout=1.5)
+            finally:
+                self.cpe.shutdown(wait=False, cancel_futures=False)
+            for res in done:
+                self.results.append((self.func_info, res.result()))
         return wrapper
 
     def get_results(self) -> list[tuple[str, Any]]:
-        pass
+        print(self.results)
+        return self.results
+
+executor = ConcurrentPoolExecutor(2)
+
+@executor.concurrent_run
+def task1(*args):
+    return sum(args)
+
+
+task1(1,2,3)
+executor.get_results()
+
