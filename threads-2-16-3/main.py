b5a6d4caed57a572b70e9fcb091a2327c61b9c23
Antonio-Par <polikarpez@gmail.com>
2025-11-25 22:30:17 +0300

    Almost done, just cache adding needed

--- threads-2-16-3/main.py
+++ threads-2-16-3/main.py
@@ -30,28 +30,47 @@ class ConcurrentPoolExecutor:
         # done, _ = fut.wait(self.futures)
         # for res in done:
         for future in self.futures:
-            self.results.append(future.result())
+            if err := future.exception():
+                self.results.append(repr(err))
+            else:
+                self.results.append(future.result())
+        return list(zip(self.func_info, self.results))
 
-        print(list(zip(self.func_info, self.results)))
-        return self.results
-
-
-executor = ConcurrentPoolExecutor(2)
-
+# Работа тестирующей системы
+print("\nТест №3")
+executor = ConcurrentPoolExecutor(max_workers=3)
 
 @executor.concurrent_run
 def task1(*args):
-    time.sleep(0.3)
+    print(f"Вызвали task1 c аргументами {args}\n", end="", flush=True)
+    time.sleep(0.2)
     return sum(args)
-
 
 @executor.concurrent_run
 def task2(*args):
-    time.sleep(0.1)
-    return sum(args)
+    print(f"Вызвали task2 c аргументами {args}\n", end="", flush=True)
+    time.sleep(0.2)
+    raise ValueError("Пример ошибки")
 
-
+# должны быть неблокирующие вызовы
+start_time = time.perf_counter()
 task1(1, 2, 3)
-task2(1, 10, 3)
-executor.get_results()
+task1(1, 2, 3)
+task1(1, 2, 3)
+task1(4, 5)
+task1(6, 7)
+task1(8, 9)
+task1(1, 2, 3)
+task1(4, 5)
+task2(1, 2, 3)
+task1(10, 11)
+task1(12, 13)
+task1(14, 15)
+task2(1, 2, 3)
+task1(16, 17)
+assert time.perf_counter()-start_time < 0.01, "Вызовы декорируемых функций должны быть неблокирующими!"
 
+start_time = time.perf_counter()
+for task_info, result in executor.get_results():
+    print(f"{task_info} => {result}")
+print(f"Вызов get_results занял {time.perf_counter()-start_time:.2f}")
