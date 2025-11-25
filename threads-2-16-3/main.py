a6951d499c6a0a993929c5ff0c2086ce65eeb8f8
Antony Par <polikarpez@gmail.com>
2025-11-25 22:01:01 +0300

    Уже около дела

--- threads-2-16-3/main.py
+++ threads-2-16-3/main.py
@@ -16,30 +16,42 @@ class ConcurrentPoolExecutor:
         self.futures = []
         self.results = []
         self.cpe = fut.ThreadPoolExecutor(max_workers=self.max_workers)
+
+    def __del__(self):
+        self.cpe.shutdown(wait=False)
         
     def concurrent_run(self, func: Callable) -> Callable:
         def wrapper(*args, **kwargs):
-            self.func_info = f'{func.__name__}{args}'
-            try:
-                self.futures.append(self.cpe.submit(func, *args))
-                done, _ = fut.wait(self.futures, timeout=1.5)
-            finally:
-                self.cpe.shutdown(wait=False, cancel_futures=False)
-            for res in done:
-                self.results.append((self.func_info, res.result()))
+            self.func_info.append(f'{func.__name__}{args}')
+            self.futures.append(self.cpe.submit(func, *args))
         return wrapper
 
     def get_results(self) -> list[tuple[str, Any]]:
-        print(self.results)
+        # done, _ = fut.wait(self.futures)
+        # for res in done:
+        for future in self.futures:
+            self.results.append(future.result())
+
+        print(list(zip(self.func_info, self.results)))
         return self.results
+
 
 executor = ConcurrentPoolExecutor(2)
 
+
 @executor.concurrent_run
 def task1(*args):
+    time.sleep(0.3)
     return sum(args)
 
 
-task1(1,2,3)
+@executor.concurrent_run
+def task2(*args):
+    time.sleep(0.1)
+    return sum(args)
+
+
+task1(1, 2, 3)
+task2(1, 10, 3)
 executor.get_results()
 
